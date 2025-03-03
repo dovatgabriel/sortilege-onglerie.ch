79af809ba8dcb0211fb49c6c7d8a45a4487b267b
Gabriel <gabriel@swispace.com>
2025-03-03 09:03:39 +0100

    feat: landing header

---
+++ src/components/HeaderSection.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="header-section">
+    <img src="@/assets/icon.png" alt="icon" class="icon" />
+    <h1>Sortilège onglerie</h1>
+    <span class="swiss-quality">
+      Qualité suisse
+      <img src="@/assets/swiss-flag.webp" alt="swiss-flag" class="swiss-flag" />
+    </span>
+    <span class="email">contact@sortilege-onglerie.ch</span>
+    <div class="social">
+      <i class="pi pi-instagram" />
+      <i class="pi pi-facebook" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.header-section {
+  background-color: #f8f8f8;
+  padding: 20px;
+  height: 60vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 30px;
+
+  .icon {
+    width: 200px;
+    margin-bottom: 20px;
+  }
+
+  h1 {
+    font-size: 2.5em;
+    margin: 0;
+  }
+
+  .swiss-quality {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    font-size: 1rem;
+    font-weight: 500;
+
+    .swiss-flag {
+      width: 20px;
+    }
+  }
+
+  .email {
+    font-size: 1em;
+    font-weight: 500;
+    cursor: pointer;
+    user-select: none;
+    transition: all 200ms ease;
+
+    &:hover {
+      opacity: 0.5;
+      text-decoration: underline;
+    }
+  }
+
+  .social {
+    display: flex;
+    gap: 20px;
+    font-size: 2rem;
+    margin: 0.5em 0 0 0;
+
+    i {
+      cursor: pointer;
+      user-select: none;
+      transition: all 200ms ease;
+
+      &:hover {
+        opacity: 0.5;
+      }
+    }
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,29 @@
+<script setup lang="ts">
+import HeaderSection from '@/components/HeaderSection.vue';
+</script>
+
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <HeaderSection />
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
 
-export default defineComponent({
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-});
-</script>
+body {
+  margin: 0;
+  padding: 0;
+}
+
+h1 {
+  font-family: 'Merriweather', serif;
+}
+
+* {
+  font-family: 'Montserrat', sans-serif;
+
+  &::selection {
+    background-color: #333;
+    color: #f8f8f8;
+  }
+}
+</style>
